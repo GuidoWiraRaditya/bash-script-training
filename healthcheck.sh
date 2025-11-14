8a8b945e5207ea1fd6766b1a80ae16075ba84882
Guido WSL <aditbaskoro21@gmail.com>
2025-11-14 19:17:09 +0700

    added color to some line

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 
 echo ""
-echo "Hello There"
-echo "I'm going to do some simple System Healh Check"
-echo "Health Check Started....."
+echo -e "\e[32mHello There"
+echo -e "I'm going to do some simple System Healh Check"
+echo -e "Health Check Started.....\e[0m"
 echo ""
-echo "=======Disk Usage======"
+echo -e "\e[1;33m=======Disk Usage======\e[0m"
 df -h | column -t| head -5
 echo ""
-echo "======Top Memory CPU Processes====="
+echo -e "\e[1;33m======Top Memory CPU Processes=====\e[0m"
 ps aux --sort=-%mem | head -5 | awk '{print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10}' | column -t
 echo""
-echo "=====Top CPU Processes====="
+echo -e "\e[1;33m=====Top CPU Processes=====\e[0m"
 ps aux --sort=-%cpu | head -5 | awk '{print $1, $2, $3, $4, $5, $6, $7, $8, $9, $10}' | column -t
 echo""
-echo "======System Uptime====="
+echo -e "\e[1;33m======System Uptime=====\e[0m"
 uptime
 echo
