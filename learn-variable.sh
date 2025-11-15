60b0a6b991cd5d708a1cec977b125861eee39b15
Guido WSL <aditbaskoro21@gmail.com>
2025-11-15 12:33:47 +0700

    learning branching and variable on bash

---
+++ learn-variable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+first="Hello"
+second="World"
+
+echo -e "\e[32m$first\e[0m $second"
+
+color1="Green"
+color2="Yellow"
+color3="red"
+
+echo ""
+echo "Each Color Name Will Be Shown In a Random Color"
+echo ""
+echo -e "\e[35m$color1\e[0m \e[36m$color2\e[0m \e[32m$color3\e[0m"
